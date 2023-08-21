1b42f349349d1a0f75044b6c0ca9d49fea815ed5
Membathisi Mcetywa <iamemba10gmail.com>
2023-08-21 07:57:54 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const passport = require("passport");
+const googleStrategy = require("passport-google-oauth20").Strategy;
+const app = express();
+
+passport.use(new googleStrategy());
+app.get("/", (req, res) => {
+  res.send({ hi: "there" });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
