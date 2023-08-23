4d05e805dc305e701327c25e8e5d12cbde593d58
Membathisi Mcetywa <iamemba10gmail.com>
2023-08-23 07:46:07 +0200

    feature(index,.js): added redis client

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const passport = require("passport");
 const googleStrategy = require("passport-google-oauth20").Strategy;
+const Redis = require("redis");
 const app = express();
+const client = Redis.createClient();
 
 passport.use(new googleStrategy());
 app.get("/", (req, res) => {
